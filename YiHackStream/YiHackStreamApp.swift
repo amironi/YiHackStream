8cdfce25845b83108b95f4aac25782e3da932b22
Amir Mironi <am5050@gmail.com>
2025-08-22 19:44:55 +0300

    Initial Commit

---
+++ YiHackStream/YiHackStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  YiHackStreamApp.swift
+//  YiHackStream
+//
+//  Created by Amir Mironi on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct YiHackStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YiHackStreamTests/YiHackStreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  YiHackStreamTests.swift
+//  YiHackStreamTests
+//
+//  Created by Amir Mironi on 22/08/2025.
+//
+
+import Testing
+@testable import YiHackStream
+
+struct YiHackStreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
